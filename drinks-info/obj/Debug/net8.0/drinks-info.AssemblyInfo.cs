53400573c887645e32d82e34aa825435d2518a73
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-04-09 18:00:38 +0300

    project finish

--- drinks-info/obj/Debug/net8.0/drinks-info.AssemblyInfo.cs
+++ drinks-info/obj/Debug/net8.0/drinks-info.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("drinks-info")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a556abc89288d0b67084e4ddfc3f977af57915b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("drinks-info")]
 [assembly: System.Reflection.AssemblyTitleAttribute("drinks-info")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
